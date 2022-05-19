0a72d0d491a45029d30338a3232e9d577419761b
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-05-19 13:56:58 +0300

    Loop exercise: repeating numbers

---
+++ Loops/Loops.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Loops
+{
+    class Loops
+    {
+        static void Main(string[] args)
+        {
+            string nums = "";
+            for (int i = 0; i < Convert.ToInt16(args[0]); i++)
+            {
+                nums = nums + Convert.ToString(i + 1);
+                Console.WriteLine(nums);
+            }
+        }
+    }
+}
