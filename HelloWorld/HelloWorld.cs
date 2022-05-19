913e244e1118fa5a14eeb50f3a2f6cf077726b6f
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-05-19 09:40:12 +0300

    Converted to ask users favourite fruit

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,10 +1,16 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     class HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string answer;
+
+            Console.Write("What is your favourite fruit? ");
+            answer = Console.ReadLine();
+            Console.WriteLine("Your favourite fruit is " + answer);
         }
     }
 }
