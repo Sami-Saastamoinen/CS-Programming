80257d95a4223e26080c427260ed09addf07b35c
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-05-23 11:58:47 +0300

    Loop exercise: sum user input until 200 is reached

--- Loops/Loops.cs
+++ Loops/Loops.cs
@@ -6,12 +6,16 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            string nums = "";
-            for (int i = 0; i < Convert.ToInt16(args[0]); i++)
+            int sum = 0;
+            int input = 0;
+
+            while (sum < 200)
             {
-                nums = nums + Convert.ToString(i + 1);
-                Console.WriteLine(nums);
+                Console.Write("Sum is now {0}. Input: ", sum);
+                input = Convert.ToInt32(Console.ReadLine());
+                sum += input;
             }
+            Console.WriteLine("Reached 200. Sum was {0}.", sum);
         }
     }
 }
