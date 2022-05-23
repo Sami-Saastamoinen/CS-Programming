3a007d37d6a79ed91fbdf033966fca845165c0ae
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-05-23 14:27:57 +0300

    File exercise: read from file given as cmdline arg

---
+++ FileMgmt/FileMgmt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileMgmt
+{
+    class FileMgmt
+    {
+        static void Main(string[] args)
+        {
+            string fileName, content;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No file name given!");
+                return;
+            }
+            else
+            {
+                fileName = args[0];
+            }
+            if (File.Exists(fileName))
+            {
+                content = File.ReadAllText(fileName);
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("File not found!");
+            }
+        }
+    }
+}
