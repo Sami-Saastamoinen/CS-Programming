23172c74520e78b8b0584020dfcfba436cbcab3b
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-05-23 14:04:35 +0300

    List exercise: list of positive numbers

---
+++ Lists/Lists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lists
+{
+    class Lists
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            List<int> nums = new List<int>();
+
+            while (input >= 0)
+            {
+                Console.Write("Enter a number (negative to stop): ");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input >= 0)
+                {
+                    nums.Add(input);
+                }
+            }
+
+            Console.Write("Numbers that you entered: ");
+            foreach (int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
